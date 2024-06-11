854877417c41f42efc537fe805a07052df252a3b
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-11 14:31:06 +0900

    add workflow

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+## Terraform configuration
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.54.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = var.backend_resource_group_name
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = var.backend_key
+  }
+
+  required_version = ">= 1.2.6"
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.key_vault_resource_group_name
+  location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                      = var.key_vault_name
+  resource_group_name       = var.key_vault_resource_group_name
+  location                  = var.location
+  sku_name                  = "standard"
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  enable_rbac_authorization = true
+}
+
+resource "azurerm_role_assignment" "secret_officer_current" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "secret_officer" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = var.product_service_connection_object_id
+}
