06fb6e58d27c05e5a3889b2e8ddd24143cab45bd
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-15 23:51:48 +0900

    add ts files

--- terraform/env/develop/variables.tf
+++ terraform/env/develop/variables.tf
@@ -8,11 +8,6 @@ variable "tenant_id" {
   type        = string
 }
 
-variable "key_vault_name" {
-  description = "The name of the key vault."
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "The name of the resource group in which the key vault is located."
   type        = string
@@ -35,5 +30,25 @@ variable "product_principal_id" {
 
 variable "container_principal_id" {
   description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "backend_resource_group_name" {
+  description = "The name of the resource group in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_storage_account_name" {
+  description = "The name of the storage account in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_container_name" {
+  description = "The name of the container in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_key" {
+  description = "The key to access the backend resources."
   type        = string
 }
--- terraform/modules/KeyVault/main.tf
+++ terraform/modules/KeyVault/main.tf
@@ -1,59 +1,38 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-## Terraform configuration
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.54.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = var.backend_resource_group_name
-    storage_account_name = var.backend_storage_account_name
-    container_name       = var.backend_container_name
-    key                  = var.backend_key
-  }
-
-  required_version = ">= 1.2.6"
-}
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.key_vault_resource_group_name
-  location = var.location
-}
-
 resource "azurerm_key_vault" "kv" {
   name                      = var.key_vault_name
-  resource_group_name       = var.key_vault_resource_group_name
+  resource_group_name       = var.resource_group_name
   location                  = var.location
   sku_name                  = "standard"
-  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  tenant_id                 = var.tenant_id
   enable_rbac_authorization = true
 }
 
-resource "azurerm_role_assignment" "secret_officer_current" {
+resource "azurerm_role_assignment" "backend_app_secret" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
+  principal_id         = var.backend_principal_id
 }
 
-resource "azurerm_role_assignment" "secret_officer" {
+resource "azurerm_role_assignment" "backend_app_cert" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Certificate Officer"
+  principal_id         = var.backend_principal_id
+}
+
+resource "azurerm_role_assignment" "base_app_secret" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = var.product_service_connection_object_id
+  principal_id         = var.base_principal_id
+}
+
+resource "azurerm_role_assignment" "product_app_secret" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = var.backend_principal_id
+}
+
+resource "azurerm_role_assignment" "container_app_secret" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = var.container_principal_id
 }
