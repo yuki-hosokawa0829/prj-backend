f9a1b759f906eb46a62ff14f4472fdb6a1dd75d8
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-17 22:18:18 +0900

    modify Key Vault Certificates Officer

--- terraform/modules/kv/main.tf
+++ terraform/modules/kv/main.tf
@@ -9,30 +9,30 @@ resource "azurerm_key_vault" "kv" {
 
 resource "azurerm_role_assignment" "backend_app_secret" {
   scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secret Officer"
+  role_definition_name = "Key Vault Secrets Officer"
   principal_id         = var.backend_principal_id
 }
 
 resource "azurerm_role_assignment" "backend_app_cert" {
   scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Certificate Officer"
+  role_definition_name = "Key Vault Certificates Officer"
   principal_id         = var.backend_principal_id
 }
 
 resource "azurerm_role_assignment" "base_app_secret" {
   scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secret Officer"
+  role_definition_name = "Key Vault Secrets Officer"
   principal_id         = var.base_principal_id
 }
 
 resource "azurerm_role_assignment" "product_app_secret" {
   scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secret Officer"
+  role_definition_name = "Key Vault Secrets Officer"
   principal_id         = var.backend_principal_id
 }
 
 resource "azurerm_role_assignment" "container_app_secret" {
   scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secret Officer"
+  role_definition_name = "Key Vault Secrets Officer"
   principal_id         = var.container_principal_id
 }
