dce21191db17ca7a461ada86684958bc9c1de485
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-18 15:52:35 +0900

    add prod tf files

---
+++ terraform/env/production/main.tf
@@ -0,0 +1,48 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+## Terraform configuration
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.54.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = var.backend_resource_group_name
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = var.backend_key
+    use_azuread_auth     = true
+  }
+
+  required_version = ">= 1.2.6"
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+locals {
+  name_prefix = "prod"
+}
+
+module "keyvault" {
+  source                 = "../../modules/kv"
+  key_vault_name         = "${local.name_prefix}keyvaultcontainer"
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  tenant_id              = var.tenant_id
+  backend_principal_id   = var.backend_principal_id
+  base_principal_id      = var.base_principal_id
+  product_principal_id   = var.product_principal_id
+  container_principal_id = var.container_principal_id
+}
---
+++ terraform/env/production/variables.tf
@@ -0,0 +1,54 @@
+variable "location" {
+  description = "The Azure Region in which all resources will be created."
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The Azure AD tenant ID."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the key vault is located."
+  type        = string
+}
+
+variable "backend_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "base_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "product_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "container_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "backend_resource_group_name" {
+  description = "The name of the resource group in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_storage_account_name" {
+  description = "The name of the storage account in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_container_name" {
+  description = "The name of the container in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_key" {
+  description = "The key to access the backend resources."
+  type        = string
+}
