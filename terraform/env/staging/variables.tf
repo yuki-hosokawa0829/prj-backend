2dea2ab27e7d5c258b470fc781c910a9cdd67c24
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-18 15:28:50 +0900

    modify variables

--- terraform/env/staging/variables.tf
+++ terraform/env/staging/variables.tf
@@ -32,3 +32,23 @@ variable "container_principal_id" {
   description = "The object ID of the service principal that will access the key vault."
   type        = string
 }
+
+variable "backend_resource_group_name" {
+  description = "The name of the resource group in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_storage_account_name" {
+  description = "The name of the storage account in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_container_name" {
+  description = "The name of the container in which the backend resources are located."
+  type        = string
+}
+
+variable "backend_key" {
+  description = "The key to access the backend resources."
+  type        = string
+}
