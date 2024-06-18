9a1ccd67dbed1e8b7d7fef197ef994b7ff9452f7
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-18 15:45:37 +0900

    modify kv module

--- terraform/modules/kv/main.tf
+++ terraform/modules/kv/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_role_assignment" "base_app_secret" {
 resource "azurerm_role_assignment" "product_app_secret" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = var.backend_principal_id
+  principal_id         = var.product_principal_id
 }
 
 resource "azurerm_role_assignment" "container_app_secret" {
