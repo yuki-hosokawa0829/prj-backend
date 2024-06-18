34aaf5ed0c2738168228beb11eaa1cbd136b0d48
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-18 15:13:58 +0900

    add tf files

---
+++ terraform/env/staging/variables.tf
@@ -0,0 +1,39 @@
+variable "location" {
+  description = "The Azure Region in which all resources will be created."
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The Azure AD tenant ID."
+  type        = string
+}
+
+variable "key_vault_name" {
+  description = "The name of the key vault."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the key vault is located."
+  type        = string
+}
+
+variable "backend_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "base_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "product_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
+
+variable "container_principal_id" {
+  description = "The object ID of the service principal that will access the key vault."
+  type        = string
+}
