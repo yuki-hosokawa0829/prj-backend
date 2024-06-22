d512e89d06049366b0283ca8aca1601d74c15387
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-22 23:44:25 +0900

    set depends_on

--- terraform/modules/kv/main.tf
+++ terraform/modules/kv/main.tf
@@ -1,32 +1,3 @@
-resource "azurerm_key_vault" "kv" {
-  name                       = var.key_vault_name
-  resource_group_name        = var.resource_group_name
-  location                   = var.location
-  sku_name                   = "standard"
-  tenant_id                  = var.tenant_id
-  enable_rbac_authorization  = true
-  soft_delete_retention_days = 7
-}
-
-resource "terraform_data" "secret_name_list" {
-  input = split(",", var.secret_name_list)
-}
-
-resource "terraform_data" "secret_value_list" {
-  input = split(",", replace(var.secret_value_list, "\r", ""))
-}
-
-resource "azurerm_key_vault_secret" "secret" {
-  count        = length(terraform_data.secret_name_list)
-  name         = terraform_data.secret_name_list.output[count.index]
-  value        = terraform_data.secret_value_list.output[count.index]
-  key_vault_id = azurerm_key_vault.kv.id
-}
-
-#output "secrets" {
-#  value = azurerm_key_vault_secret.secret[*].value
-#}
-
 resource "azurerm_role_assignment" "backend_app_secret_officer" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
@@ -62,3 +33,36 @@ resource "azurerm_role_assignment" "container_app_secret_user" {
   role_definition_name = "Key Vault Secrets User"
   principal_id         = var.container_principal_id
 }
+
+resource "azurerm_key_vault" "kv" {
+  name                       = var.key_vault_name
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
+  sku_name                   = "standard"
+  tenant_id                  = var.tenant_id
+  enable_rbac_authorization  = true
+  soft_delete_retention_days = 7
+}
+
+resource "terraform_data" "secret_name_list" {
+  input = split(",", var.secret_name_list)
+}
+
+resource "terraform_data" "secret_value_list" {
+  input = split(",", replace(var.secret_value_list, "\r", ""))
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  count        = length(terraform_data.secret_name_list)
+  name         = terraform_data.secret_name_list.output[count.index]
+  value        = terraform_data.secret_value_list.output[count.index]
+  key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [
+    azurerm_role_assignment.backend_app_secret_officer
+  ]
+}
+
+#output "secrets" {
+#  value = azurerm_key_vault_secret.secret[*].value
+#}
