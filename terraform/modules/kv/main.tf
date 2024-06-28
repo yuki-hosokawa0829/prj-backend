7d19f12def73497480eb68735c02f771ad7ad46d
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-28 12:38:08 +0900

    add base_app_secret_user

--- terraform/modules/kv/main.tf
+++ terraform/modules/kv/main.tf
@@ -43,6 +43,13 @@ resource "azurerm_role_assignment" "base_app_cert_user" {
   principal_id         = var.base_principal_id
 }
 
+resource "azurerm_role_assignment" "base_app_secret_user" {
+  count                = var.project_suffix == "base" ? 1 : 0
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = var.base_principal_id
+}
+
 resource "azurerm_role_assignment" "base_app_secret_officer" {
   count                = var.project_suffix == "container" ? 1 : 0
   scope                = azurerm_key_vault.kv.id
